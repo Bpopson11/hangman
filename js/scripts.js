c9f760efc759eed54a404b22d6557544fa02217c
Brianna Popson and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-27 11:11:36 -0800

    refactored first two specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,45 @@
-function chooseWord() {
-  var words = ["cat"];
-  var word = words[Math.floor(Math.random()*words.length)];
+function HangMan(words, guessedLetters, score) {
+  this.words = ["cat", "dog"];
+  this.guessedLetters = [];
+  this.score = 10;
+}
+
+HangMan.prototype.chooseWord = function(){
+  var word = this.words[Math.floor(Math.random()*this.words.length)];
   return word;
-};
+}
 
-function splitWord() {
-  var words = ["cat"];
-  var wordSplit = [];
-  var word = words[Math.floor(Math.random()*words.length)];
-  wordSplit = word.split([]);
-  return wordSplit;
-};
 
-function findLetter() {
-  var testArray = ["c", "a", "t"];
-  for (var i = 0; i <= testArray.length; i++) {
-    if (testArray[i] === "a") {
-      return ["a"];
-    };
-  };
-};
+HangMan.prototype.splitWord = function() {
+  var word = this.words[Math.floor(Math.random()*this.words.length)];
+  return word.split([]);
+}
+//
+// function splitWord() {
+//   var words = ["cat"];
+//   var wordSplit = [];
+//   var word = words[Math.floor(Math.random()*words.length)];
+//   wordSplit = word.split([]);
+//   return wordSplit;
+// };
+//
+// function findLetter(letterArray) {
+//   //var testArray = ["c", "a", "t"];
+//   for (var i = 0; i <= letterArray.length; i++) {
+//     console.log(letterArray[i]);
+//
+//     // if (testArray[i] === "a") {
+//     //   return [i];
+//     // };
+//   };
+//
+// };
+//
+// function findLetters(){
+//   var testArray = ['c', 'a', 'n', 'a', 'l']
+//   for (var i = 0; i <= testArray.length; i++) {
+//     if (testArray[i] === 'a') {
+//       return ['a', 'a']
+//     };
+//   };
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,21 @@
-describe('chooseWord', function(){
+describe('HangMan', function(){
   it("will choose a random word from an array", function(){
-  expect(chooseWord("cat")).to.equal("cat"); //ADDED MORE WORDS TO THE ARRAY AND THE SPEC ONLY PASSED WHEN IT CHOSE "CAT" AT RANDOM
+    var testWord = new HangMan(["cat"], [], 10);
+    expect(testWord.chooseWord()).to.equal("cat"); //ADDED MORE WORDS TO THE ARRAY AND THE SPEC ONLY PASSED WHEN IT CHOSE "CAT" AT RANDOM
   });
 
   it("will choose a random word from an array and split it into individual letters", function() {
-    expect(splitWord("cat")).to.eql(["c", "a", "t"]);
-
+    var testWord = new HangMan(["cat"], [], 10);
+    expect(testWord.splitWord()).to.eql(["c", "a", "t"]);
   });
 });
 
 describe('findLetter', function() {
   it("will identify a letter in an array", function() {
     expect(findLetter(["c", "a", "t"])).to.eql(["a"]);
+  }); //look into using charAt() to return the actual character based on the array index
+
+  it("will identify multiples of the same letter in an array", function(){
+    expect(findLetters(['c', 'a', 'n', 'a', 'l'])).to.eql(['a', 'a']);
   });
 });
