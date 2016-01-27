10aa3f59e88d88ac5e8b587ed4738492f5d503c4
Brianna Popson and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-27 08:49:06 -0800

    first spec passed, second spec failed

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function chooseWord() {
+  var words = ["cat"]
+  var word = words[Math.floor(Math.random()*words.length)];
+  return word;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe('chooseWord', function(){
   it("will choose a random word from an array", function(){
-  expect(testWord("cat")).to.equal("cat");
+  expect(chooseWord("cat")).to.equal("cat"); //ADDED MORE WORDS TO THE ARRAY AND THE SPEC ONLY PASSED WHEN IT CHOSE "CAT" AT RANDOM
+  });
+
+  it("will choose a random word from an array and split it into individual letters", function() {
+    expect(chooseWord("cat")).to.eql(["c", "a", "t"]);
+
   });
 });
